8c9a63ea9454cdf7a9b9e20f773a90b66ea9d6f5
Jonathan Felicity <jonathanfelicity@mail.com>
2022-08-30 10:17:03 +0100

    Deployment script course aded

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts'); 
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
 
 const app = express();
 
@@ -17,6 +20,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressLayouts); 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
