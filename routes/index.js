4c2c52477334724f9df09441537f7066cd5139c8
Jonathan Felicity <jonathanfelicity@mail.com>
2022-08-30 10:48:47 +0100

    Layouts added

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,31 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('pages.home', { title: 'Home' });
 });
-
+/* GET home page. */
+router.get('/events', function(req, res, next) {
+  res.render('pages.events', { title: 'Shop' });
+});
 /* GET home page. */
 router.get('/shop', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages.shop', { title: 'Shop' });
 });
 
 /* GET home page. */
 router.get('/blog', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages.blog', { title: 'Blog' });
 });
 
 
 /* GET home page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages.about', { title: 'About' });
 });
 
 /* GET conatact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Express' });
+  res.render('pages.contact', { title: 'Contact' });
 });
 
 module.exports = router;
