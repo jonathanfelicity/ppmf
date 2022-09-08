88d47dcc76053681d35a162b97576ffb3b418ab1
Jonathan Felicity <jonathanfelicity@mail.com>
2022-09-07 23:19:42 -0400

    404 bug fixed

--- app.js
+++ app.js
@@ -28,10 +28,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('*', function(req, res){
+  res.status(404).render('errors/404', {title:'Page Not Found'});
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
